1060b7468f112fb894c589b6c188881268b33987
Alexandre Jota Quaresma <20232ewbj0300@belojardim.ifpe>
2025-12-03 14:32:27 -0300

    fix

--- src/client/Client.java
+++ src/client/Client.java
@@ -14,6 +14,8 @@ public class Client {
         CeilingFan ceilingFan = new CeilingFan("Living Room");
         GarageDoor garageDoor = new GarageDoor("Garage");
         Sound stereo = new Sound("Living Room");
+        Hottub hottub = new Hottub();              // NOVO
+        SecurityControl security = new SecurityControl(); // NOVO
 
         LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
         LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
@@ -25,12 +27,21 @@ public class Client {
         GarageDoorDownCommand garageDoorDown = new GarageDoorDownCommand(garageDoor);
         SoundOnCommand stereoOn = new SoundOnCommand(stereo);
         SoundOffCommand stereoOff = new SoundOffCommand(stereo);
+        
+        // Comandos NOVOS
+        HottubOnCommand hottubOn = new HottubOnCommand(hottub);
+        HottubOffCommand hottubOff = new HottubOffCommand(hottub);
+        SecurityArmCommand securityArm = new SecurityArmCommand(security);
+        SecurityDisarmCommand securityDisarm = new SecurityDisarmCommand(security);
 
+        // Configurando Slots
         remote.setCommand(0, livingRoomLightOn, livingRoomLightOff);
         remote.setCommand(1, livingRoomTVOn, livingRoomTVOff);
         remote.setCommand(2, ceilingFanHigh, ceilingFanOff);
         remote.setCommand(3, stereoOn, stereoOff);
         remote.setCommand(4, garageDoorUp, garageDoorDown);
+        remote.setCommand(5, hottubOn, hottubOff);         // NOVO
+        remote.setCommand(6, securityArm, securityDisarm); // NOVO
 
         // 2. Usando o controle (Isso vai salvar no disco)
         System.out.println("\n--- USANDO O CONTROLE REMOTO (Gravando no Histórico) ---");
@@ -43,7 +54,13 @@ public class Client {
         remote.onButtonWasPushed(3); 
         remote.offButtonWasPushed(3); 
         remote.onButtonWasPushed(4); 
-        remote.offButtonWasPushed(4); 
+        remote.offButtonWasPushed(4);
+        
+        // Testando os novos dispositivos
+        remote.onButtonWasPushed(5); // Banheira Liga
+        remote.offButtonWasPushed(5); // Banheira Desliga
+        remote.onButtonWasPushed(6); // Alarme Arma
+        remote.offButtonWasPushed(6); // Alarme Desarma
 
         // 3. Simulando a falha e recuperação
         System.out.println("\n\n--- SIMULANDO PANE ELÉTRICA (Reiniciando Sistema) ---");
